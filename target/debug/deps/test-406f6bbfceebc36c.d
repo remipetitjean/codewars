4d7931960a4d59d0d5ed9121a6ba8236d93c1109
remipetitjean <remi.petitjean@gmail.com>
2021-03-23 23:43:38 +0000

    moving code to root

---
+++ target/debug/deps/test-406f6bbfceebc36c.d
@@ -0,0 +1,5 @@
+/home/remi/workspace/tools/codewars/target/debug/deps/test-406f6bbfceebc36c: src/bin/test.rs
+
+/home/remi/workspace/tools/codewars/target/debug/deps/test-406f6bbfceebc36c.d: src/bin/test.rs
+
+src/bin/test.rs:
---
+++ target/debug/test.d
@@ -0,0 +1,1 @@
+/home/remi/workspace/tools/codewars/target/debug/test: /home/remi/workspace/tools/codewars/src/bin/test.rs
